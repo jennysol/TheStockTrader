25036b5eb7478845da119a6a14ec9038c143ad71
jennysol <jennyssoliver@gmail.com>
2021-03-09 20:29:48 -0300

    Starting components

---
+++ stock-trader/src/components/portfolio/Stock.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Action (Portfolio)</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ stock-trader/src/components/stockes/Stockes.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Actions</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
