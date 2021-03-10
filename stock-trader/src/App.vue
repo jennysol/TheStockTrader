6b9297eb123170c65476664bdaeccf9ba6d168a4
jennysol <jennyssoliver@gmail.com>
2021-03-09 21:36:50 -0300

    Adding Header and Navigation

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -1,20 +1,23 @@
 <template>
-	<v-app>
-		<v-content>
-      <v-container>
-        <router-view></router-view>
-      </v-container>
-		</v-content>
-	</v-app>
+  <v-app>
+    <Header/>
+      <v-content>
+        <HelloWorld/>
+      </v-content>
+  </v-app>
 </template>
 
 <script>
+import Header from './components/Header'
 export default {
-
+  name: 'App',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
-
