d61ccdce8d252cb70b34b16b988115e0a4f2996d
jennysol <jennyssoliver@gmail.com>
2021-03-14 22:38:01 -0300

    Transitions

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -21,3 +21,23 @@ export default {
   },
 }
 </script>
+
+<style>
+  @keyframes slide-in {
+    from { transform: translateY(-30px); opacity: 0; }
+    to { transform: translateY(0px); opacity: 1; }
+  }
+
+  @keyframes slide-out {
+    from { transform: translateY(0px); opacity: 1; }
+    to { transform: translateY(-30px); opacity: 0; }
+  }
+
+  .slide-enter-active {
+    animation: slide-in 0.3s ease;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 0.3s ease;
+  }
+</style>
